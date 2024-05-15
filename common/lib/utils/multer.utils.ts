7d971ebf930642c6055f9b2ee0592b2af5cac590
Nicolas Depelteau <n.depelteau@lightxinnovations.com>
2024-05-15 09:21:03 -0400

    update common lib

--- common/lib/utils/multer.utils.ts
+++ common/lib/utils/multer.utils.ts
@@ -3,7 +3,11 @@ import { Request } from "express";
 
 export class MulterUtils {
     public static filterFileExtensions(mimetypes: string[]): any {
-        return (req: Request, file: Express.Multer.File, callback: (error: Error | null, acceptFile: boolean) => void) => {
+        return (
+            req: Request,
+            file: Express.Multer.File,
+            callback: (error: Error | null, acceptFile: boolean) => void
+        ) => {
             if (!mimetypes.some((mt) => mt == file.mimetype)) {
                 return callback(new UnsupportedMediaTypeException("File extension not supported"), false);
             }
