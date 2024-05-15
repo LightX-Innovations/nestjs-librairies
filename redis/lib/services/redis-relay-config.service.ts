1e27b46e14bacd373f2f4ebe72f482c9084bc6ab
Nicolas Depelteau <n.depelteau@lightxinnovations.com>
2024-05-15 08:22:15 -0400

    update redis

--- redis/lib/services/redis-relay-config.service.ts
+++ redis/lib/services/redis-relay-config.service.ts
@@ -8,9 +8,10 @@ export class RedisRelayConfigService extends RedisConfigService {
         this.connectionString = process.env.REDIS_RELAY_CONNECTION_STRING;
         this.host = process.env.REDIS_RELAY_HOST ?? "127.0.0.1";
         this.password = process.env.REDIS_RELAY_PASSWORD;
-        this.port = +process.env.REDIS_RELAY_PORT ?? 6379;
-        this.database = process.env.REDIS_RELAY_DATABASE ? +process.env.REDIS_DATABASE : 0;
+        this.port = process.env.REDIS_RELAY_PORT != undefined ? +process.env.REDIS_RELAY_PORT : 6379;
+        this.database = process.env.REDIS_RELAY_DATABASE != undefined ? +process.env.REDIS_RELAY_DATABASE : 0;
         this.useTLS = process.env.REDIS_RELAY_USE_TLS === "true";
-        this.lpushBlockSize = process.env.REDIS_RELAY_LPUSH_BLOCK_SIZE ? +process.env.REDIS_LPUSH_BLOCK_SIZE : 250;
+        this.lpushBlockSize =
+            process.env.REDIS_RELAY_LPUSH_BLOCK_SIZE != undefined ? +process.env.REDIS_RELAY_LPUSH_BLOCK_SIZE : 250;
     }
 }
