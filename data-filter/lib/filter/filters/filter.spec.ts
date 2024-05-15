d998402e0365e5967fcfd55ad1a5cae9865f62bd
Nicolas Depelteau <n.depelteau@lightxinnovations.com>
2024-05-15 09:33:24 -0400

    update data filter lib

--- data-filter/lib/filter/filters/filter.spec.ts
+++ data-filter/lib/filter/filters/filter.spec.ts
@@ -1,112 +1,114 @@
-import { Filter } from "./filter";
-import { TextFilter } from "./text.filter";
-import { FilterOperatorTypes } from "../operators";
 import { col, fn, Op, where, WhereOptions } from "sequelize";
+import { FilterOperatorTypes } from "../operators";
+import { TextFilter } from "./text.filter";
 
 describe("Filter", () => {
     describe("getWhereOptions", () => {
         it("should return a valid filter config", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             });
             const options = await filter.getWhereOptions({
                 id: "test",
                 value: "test",
-                operation: FilterOperatorTypes.Equal
+                operation: FilterOperatorTypes.Equal,
             });
             expect(options).toBeDefined();
             expect(options).toStrictEqual<WhereOptions>({
-                test: "test"
+                test: "test",
             });
         });
 
         it("with custom name should return a valid filter config", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             });
-            const options = await filter.getWhereOptions({
-                id: "test",
-                value: "test",
-                operation: FilterOperatorTypes.Equal
-            }, "customName");
+            const options = await filter.getWhereOptions(
+                {
+                    id: "test",
+                    value: "test",
+                    operation: FilterOperatorTypes.Equal,
+                },
+                "customName"
+            );
             expect(options).toBeDefined();
             expect(options).toStrictEqual<WhereOptions>({
-                customName: "test"
+                customName: "test",
             });
         });
 
         it("with path should return a valid filter config", async () => {
             const filter = new TextFilter({
                 attribute: "test",
-                path: "path.to.attribute"
+                path: "path.to.attribute",
             });
             const options = await filter.getWhereOptions({
                 id: "test",
                 value: "test",
-                operation: FilterOperatorTypes.Equal
+                operation: FilterOperatorTypes.Equal,
             });
             expect(options).toBeDefined();
             expect(options).toStrictEqual<WhereOptions>({
-                "$path.to.attribute.test$": "test"
+                "$path.to.attribute.test$": "test",
             });
         });
 
         it("with custom operator should return a valid filter config", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             }).addOperators({
                 name: "test",
-                operator: FilterOperatorTypes.Contains
+                operator: FilterOperatorTypes.Contains,
             });
             const options = await filter.getWhereOptions({
                 id: "test",
                 value: "test",
-                operation: "test"
+                operation: "test",
             });
             expect(options).toBeDefined();
             expect(options).toStrictEqual<WhereOptions>({
                 test: {
-                    [Op.like]: "%test%"
-                }
+                    [Op.like]: "%test%",
+                },
             });
         });
 
         it("with having defined should return null", async () => {
             const filter = new TextFilter({
                 attribute: "test",
-                having:fn("count", col("test.email"))
+                having: fn("count", col("test.email")),
             });
             const options = await filter.getWhereOptions({
                 id: "test",
                 value: "test",
-                operation: "test"
+                operation: "test",
             });
-            expect(options).toBeNull();
+            expect(options).toBeFalsy();
         });
     });
 
     describe("getHavingOptions", () => {
         it("with no having defined should return null", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             });
             const options = await filter.getHavingOptions({
                 id: "test",
                 value: "test",
-                operation: FilterOperatorTypes.Equal
+                operation: FilterOperatorTypes.Equal,
             });
-            expect(options).toBeNull();
+            expect(options).toBeFalsy();
         });
 
         it("with having defined should return a valid filter config", async () => {
             const filter = new TextFilter({
                 attribute: "test",
-                having: fn("count", col("test.email"))
+                having: fn("count", col("test.email")),
             });
             const options = await filter.getHavingOptions({
                 id: "test",
                 value: 0,
-                operation: FilterOperatorTypes.Equal
+                operation: FilterOperatorTypes.Equal,
             });
             expect(options).toBeDefined();
             expect(options).toStrictEqual(where(fn("count", col("test.email")), 0 as any));
@@ -114,22 +116,24 @@ describe("Filter", () => {
 
         it("with custom operator should return a valid filter config", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             }).addOperators({
                 name: "none",
                 having: where(fn("count", col("system.place.owners.person.email")), {
-                    [Op.lte]: 0
-                })
+                    [Op.lte]: 0,
+                }),
             });
             const options = await filter.getHavingOptions({
                 id: "test",
                 value: null,
-                operation: "none"
+                operation: "none",
             });
             expect(options).toBeDefined();
-            expect(options).toStrictEqual(where(fn("count", col("system.place.owners.person.email")), {
-                [Op.lte]: 0
-            }));
+            expect(options).toStrictEqual(
+                where(fn("count", col("system.place.owners.person.email")), {
+                    [Op.lte]: 0,
+                })
+            );
         });
     });
 
@@ -138,31 +142,31 @@ describe("Filter", () => {
 
         it("with enabled undefined it should return the config", async () => {
             const filter = new TextFilter({
-                attribute: "test"
+                attribute: "test",
             });
             filter.translateService = translationService;
             const config = await filter.getConfig("", null, {});
-            expect(config).toBeDefined()
-        })
+            expect(config).toBeDefined();
+        });
 
         it("with enabled returning true it should return the config", async () => {
             const filter = new TextFilter({
                 attribute: "test",
-                enabled: async ({ user, request }) => true
+                enabled: async ({ user, request }) => true,
             });
             filter.translateService = translationService;
             const config = await filter.getConfig("", null, {});
-            expect(config).toBeDefined()
-        })
+            expect(config).toBeDefined();
+        });
 
         it("with enabled returning false it should return null", async () => {
             const filter = new TextFilter({
                 attribute: "test",
-                enabled: async ({ user, request }) => false
+                enabled: async ({ user, request }) => false,
             });
             filter.translateService = translationService;
             const config = await filter.getConfig("", null, {});
-            expect(config).toBeNull()
-        })
-    })
+            expect(config).toBeNull();
+        });
+    });
 });
